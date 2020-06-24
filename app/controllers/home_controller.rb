fef3c12bcfb7ecd81a4b2e194b1e03fdb921dace
wilson <wilsonkhoo93@gmail.com>
2020-06-24 17:42:28 +0800

    Test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
-    @customers = ShopifyAPI::Customer.find(:all, params: { limit: 10 })
-    client = ShopifyAPI::GraphQL.client
-    @shop = client.query(Shopify::Shop::SHOP_QUERY)
+    # @customers = ShopifyAPI::Customer.find(:all, params: { limit: 10 })
+    # client = ShopifyAPI::GraphQL.client
+    # @shop = client.query(Shopify::Shop::SHOP_QUERY)
   end
 end
